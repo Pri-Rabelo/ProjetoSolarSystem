0e3317d81f20c0a8574e61052ee50a3a42c12931
Pri-Rabelo <priscilarabelo_2002@yahoo.com.br>
2022-01-26 16:27:15 -0300

    adiciona requisito 7

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header'; // requisito 1
 import SolarSystem from './components/SolarSystem'; // Requisito 2
 // import Title from './components/Title';  alterado no requisito 4 pro arquivo SolarSystem.js
 // import PlanetCard from './components/PlanetCard'; alterado no requisito 6 pro arquivo SolarSystem.js
+import Missions from './components/Missions';
 
 class App extends React.Component {
   render() {
@@ -10,6 +11,7 @@ class App extends React.Component {
       <div>
         <Header />
         <SolarSystem />
+        <Missions />
       </div>
     );
   }
